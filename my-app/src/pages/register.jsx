49586af5b42caf09359828ce656082c886da8ef7
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-09-23 11:47:07 +0200

    Adding codes of registration form

--- my-app/src/pages/register.jsx
+++ my-app/src/pages/register.jsx
@@ -1,21 +1,30 @@
+import "./register.css";
+
 function Register() {
-    return (
-        <div>
-            <fieldset>
-                <legend>Registration Form</legend>
-                <label>First Name</label>
-                <input type="text"  placeholder="First name" /> <br />
-                <label>Last Name</label>
-                <input type="text"  placeholder="Last name"/> <br />
-                <label>Email</label>
-                <input type="text"  placeholder="Email"/> <br />
-                <label>Address</label>
-                <input type="text"  placeholder="Address"/> <br />
-                <button>Cancel</button>
-                <button>Address</button>
-                </fieldset>
+  return (
+    <div className="register">
+      <fieldset className="fieldset">
+        <legend>Registration Form</legend>
+
+        <label>First Name</label>
+        <input type="text" placeholder="First name" />
+
+        <label>Last Name</label>
+        <input type="text" placeholder="Last name" />
+
+        <label>Email</label>
+        <input type="email" placeholder="Email" />
+
+        <label>Address</label>
+        <input type="text" placeholder="Address" />
+
+        <div className="button">
+          <button className="btn cancel-btn">Cancel</button>
+          <button className="btn register-btn">Register</button>
         </div>
-    )
+      </fieldset>
+    </div>
+  );
 }
 
 export default Register;
