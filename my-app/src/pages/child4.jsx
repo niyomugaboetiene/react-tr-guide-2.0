4ee37ced3196eade0a8b03694f1872e3ad782557
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-07 09:44:58 +0200

    initial commit

--- my-app/src/pages/child4.jsx
+++ my-app/src/pages/child4.jsx
@@ -1,15 +1,11 @@
 function ProductList(props) {
     const productName = [props.name1, props.name2, props.name3];
     return (
-        <div>
-                {productName.map((idx, name) => (
-                   <ul key={idx}>
-                      <li>{name.name1}</li>
-                      <li>{name.name2}</li>
-                      <li>{name.name3}</li>
-                   </ul>
+        <ul>
+                {productName.map((name, idx) => (
+                    <li key={idx}>{name}</li>
                 ))}
-        </div>
+        </ul>
     )
 }
 
