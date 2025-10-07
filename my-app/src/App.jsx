3d65115f07862025f1b1addbaf57272af782391e
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-07 08:52:47 +0200

    initial commit

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,8 @@
 // import One from "./1"
 import Register from "./pages/register"
 import COunter from "./pages/counter"
-import Parent from "./pages/parent"
+// import Parent from "./pages/parent"
+import Parent from "./pages/parent1"
 function App() {
 
   return (
