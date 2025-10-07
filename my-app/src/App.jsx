93394ec67ac357dd854b12fdef31310523212a91
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-07 08:20:45 +0200

    importing parent from app.jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,13 +1,15 @@
 // import One from "./1"
 import Register from "./pages/register"
 import COunter from "./pages/counter"
+import Parent from "./pages/parent"
 function App() {
 
   return (
     <>
      {/* <One></One> */}
      {/* <Register /> */}
-     <COunter/>
+     {/* <COunter/> */}
+     <Parent/>
     </>
   )
 }
