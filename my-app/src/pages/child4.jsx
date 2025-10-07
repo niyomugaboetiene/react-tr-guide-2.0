f4fcef4c1afe6192907e35c80eb2fd5ccaadaaa7
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-07 09:36:17 +0200

    initial commit

--- my-app/src/pages/child4.jsx
+++ my-app/src/pages/child4.jsx
@@ -1,9 +1,14 @@
 function Product(props) {
+    const productName = [props.name1, props.name2, props.name3];
     return (
         <div>
             <ul>
-                <li>{}</li>
+                {productName.map((idx, name) => {
+                   <li key={idx}>{name}</li>
+                })}
             </ul>
         </div>
     )
 }
+
+export default Product;
--- my-app/src/pages/parent4.jsx
+++ my-app/src/pages/parent4.jsx
@@ -1,3 +1,10 @@
+import Product from "./child4";
 function ProductList() {
-
+   return (
+    <div>
+        <Product name1="Fruits" name2="Biscuits" name3="Tea"/>
+    </div>
+   )
 }
+
+export default ProductList;
