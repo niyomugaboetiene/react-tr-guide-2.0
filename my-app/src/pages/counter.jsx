d9bd1227dca84ed3126b77c33c10f165ef342992
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-07 08:22:34 +0200

    initial commit

--- my-app/src/pages/counter.jsx
+++ my-app/src/pages/counter.jsx
@@ -4,7 +4,7 @@ function COunter() {
     const [counter, setCounter] = useState(0);
 
     return (
-        <div>
+        <div className="min-h-screen bg-gray-600">
             <p>{counter}</p>
           <button onClick={() => setCounter(counter + 1)}>Increment</button>
           <button onClick={() => setCounter(counter -1)}>Decrement</button>
