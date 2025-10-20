51d3d1540a213f1c64036d582b752e1ed21e7d21
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-20 14:56:48 +0200

    react: fixing about routing

--- SWD5ROUTE/src/routing/Home.jsx
+++ SWD5ROUTE/src/routing/Home.jsx
@@ -2,9 +2,12 @@ import { Link } from "react-router-dom"
 
 const Home = () => {
     return (
-        <div className="min-h-screen flex from-blue-300 to-purple-400 via--300 bg-gradient-to-b">
-            <p>This is home page</p>
-            <Link to="/about">About</Link>
+        <div className="min-h-screen flex from-blue-300 to-purple-400 via--300 bg-gradient-to-bl items-center justify-center">
+            <div>
+                <p>This is home page</p>
+                 <Link to="/about">About</Link>
+            </div>
+   
         </div>
     )
 }
