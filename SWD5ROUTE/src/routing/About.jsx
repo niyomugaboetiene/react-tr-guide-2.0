f5254b5000680a8dd308c568b97f40c326442e6f
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-20 15:05:18 +0200

    react: fixing about routing

--- SWD5ROUTE/src/routing/About.jsx
+++ SWD5ROUTE/src/routing/About.jsx
@@ -1,8 +1,8 @@
 const About = () => {
     return (
         <div className="min-h-screen flex from-blue-300 to-purple-400 via-green-300 bg-gradient-to-bl items-center justify-center">
-            <div>
-                <p>This is About page</p>
+            <div className="bg-gradient-to-l from-blue-200 to-purple-200 via-green-200 p-7 w-[20%] rounded-xl shadow-2xl">
+                <p lassName="text-center text-xl font-light">This is About page</p>
             </div>
         </div>
     )
