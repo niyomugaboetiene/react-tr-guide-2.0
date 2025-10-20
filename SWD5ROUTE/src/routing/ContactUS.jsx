977537b8db40480ff582f38b7a22e01208da298c
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-20 14:37:09 +0200

    react: fixing about routing

---
+++ SWD5ROUTE/src/routing/ContactUS.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+const About = () => {
+    return (
+        <div>
+            <p>This is About page</p>
+        </div>
+    )
+}
+
+export default About
--- SWD5ROUTE/src/routing/Home.jsx
+++ SWD5ROUTE/src/routing/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
     return (
         <div>
             <p>This is home page</p>
+            <Link to="/about">About</Link>
         </div>
     )
 }
