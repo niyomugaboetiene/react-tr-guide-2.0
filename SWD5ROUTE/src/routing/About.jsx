4620dd8f62411e456bbb62bda686e5f2bc89f67b
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-20 15:04:22 +0200

    react: fixing about routing

--- SWD5ROUTE/src/routing/About.jsx
+++ SWD5ROUTE/src/routing/About.jsx
@@ -1,7 +1,9 @@
 const About = () => {
     return (
-        <div>
-            <p>This is About page</p>
+        <div className="min-h-screen flex from-blue-300 to-purple-400 via-green-300 bg-gradient-to-bl items-center justify-center">
+            <div>
+                <p>This is About page</p>
+            </div>
         </div>
     )
 }
