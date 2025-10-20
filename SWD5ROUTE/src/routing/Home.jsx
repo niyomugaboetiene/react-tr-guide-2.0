5188c1b54d1cc9caa0087cac2214cd305421a5a9
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-20 15:00:01 +0200

    react: fixing about routing

--- SWD5ROUTE/src/routing/Home.jsx
+++ SWD5ROUTE/src/routing/Home.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom"
 const Home = () => {
     return (
         <div className="min-h-screen flex from-blue-300 to-purple-400 via-green-300 bg-gradient-to-bl items-center justify-center">
-            <div className="bg-gradient-to-l from-blue-200 to-purple-200 ">
-                <p>This is home page</p>
+            <div className="bg-gradient-to-l from-blue-200 to-purple-200 via-green-200 p-7 w-[20%] rounded-xl shadow-2xl">
+                <p className="text-center">This is home page</p>
                  <Link to="/about">About</Link>
             </div>
    
