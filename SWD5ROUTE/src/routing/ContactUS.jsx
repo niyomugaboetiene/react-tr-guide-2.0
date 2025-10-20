8a32d8fdece1a9588b697b7e812ab08afc260dfa
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-20 14:58:44 +0200

    react: fixing about routing

--- SWD5ROUTE/src/routing/ContactUS.jsx
+++ SWD5ROUTE/src/routing/ContactUS.jsx
@@ -1,6 +1,6 @@
 const Contact = () => {
     return (
-        <div className="min-h-screen bg-blue-300 flex items-center justify-center">
+        <div className="min-h-screen flex items-center justify-center from-blue-300 to-purple-400 via-green-300 bg-gradient-to-bl">
             <div className="bg-blue-200 p-12 rounded-xl shadow-xl w-[30%]">
                 <p className="text-center text-2xl font-light">This is contact page</p>
             </div>
--- SWD5ROUTE/src/routing/Home.jsx
+++ SWD5ROUTE/src/routing/Home.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom"
 
 const Home = () => {
     return (
-        <div className="min-h-screen flex from-blue-300 to-purple-400 via--300 bg-gradient-to-bl items-center justify-center">
-            <div>
+        <div className="min-h-screen flex from-blue-300 to-purple-400 via-green-300 bg-gradient-to-bl items-center justify-center">
+            <div className="bg-gradient-to-l from-blue-200 to-purple-200 ">
                 <p>This is home page</p>
                  <Link to="/about">About</Link>
             </div>
