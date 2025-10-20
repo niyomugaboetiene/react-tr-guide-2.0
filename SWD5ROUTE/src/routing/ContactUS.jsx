169fa8718b8c0274c7e0265cc079e429e5b97f56
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-20 14:52:00 +0200

    react: fixing about routing

--- SWD5ROUTE/src/routing/ContactUS.jsx
+++ SWD5ROUTE/src/routing/ContactUS.jsx
@@ -1,8 +1,8 @@
 const Contact = () => {
     return (
-        <div className="min-h-screen bg-blue-300">
-            <div className="bg-blue-200 max-w-xl ms-[550px] p-6 rounded-xl shadow-xl">
-                <p className="text-center font-bold text-2xl font-light">This is contact page</p>
+        <div className="min-h-screen bg-blue-300 flex items-center justify-center">
+            <div className="bg-blue-200 max-w-xl p-6 rounded-xl shadow-xl">
+                <p className="text-center text-2xl font-light">This is contact page</p>
             </div>
         </div>
     )
