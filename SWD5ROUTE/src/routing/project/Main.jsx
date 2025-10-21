3cb1eb2ed3863754690f3b1f2734b21e7315f914
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-21 10:47:59 +0200

    react: learning about navigation

--- SWD5ROUTE/src/routing/project/Main.jsx
+++ SWD5ROUTE/src/routing/project/Main.jsx
@@ -3,8 +3,8 @@ import {  Link  } from "react-router-dom";
 const Main = () => {
     return (
         <div>
-            <div className="flex justify-center bg-gradient-to-br from-blue-300 to-green-300 via-violet-300 ">
-              <nav className="flex space-x-[100px] mt-5">
+            <div className="flex ms-[550px] justify-center bg-gradient-to-br from-green-400 to-violet-400 via-blue-400 max-w-2xl p-9">
+              <nav className="flex space-x-[100px] mt-5 justify-center">
                  <Link to="/home">Home</Link>
                  <Link to="/aboutUs">About</Link>
                  <Link to="/service">Service</Link>
