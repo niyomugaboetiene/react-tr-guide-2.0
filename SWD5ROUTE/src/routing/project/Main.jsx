1e1c54eab9c086327c7936450aedda15b7ed9938
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-21 10:34:30 +0200

    react: learning about navigation

--- SWD5ROUTE/src/routing/project/Main.jsx
+++ SWD5ROUTE/src/routing/project/Main.jsx
@@ -2,13 +2,11 @@ import {  Link  } from "react-router-dom";
 
 const Main = () => {
     return (
-        <div>
         <nav>
             <Link to="/home">Home</Link>
             <Link to="/aboutUs">About</Link>
             <Link to="/service">Service</Link>
         </nav>
-        </div>
     )
 }
 
