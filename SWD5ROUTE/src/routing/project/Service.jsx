1664e024a9a131f1416f9fb536ca7d76e858d90c
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-21 08:16:37 +0200

    react: learning about routing

---
+++ SWD5ROUTE/src/routing/project/Service.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Service = () => {
+  return (
+    <div>
+        <p>This is Service page</p>
+        <p>The page which contain our service to our customers</p>
+    </div>
+  )
+}
+
+export default Service
