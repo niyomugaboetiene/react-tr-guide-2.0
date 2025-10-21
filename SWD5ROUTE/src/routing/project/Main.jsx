cc853d3a817288a502eab9312ff5a3fced5e68e2
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-21 10:56:13 +0200

    react: learning about navigation

--- SWD5ROUTE/src/routing/project/Main.jsx
+++ SWD5ROUTE/src/routing/project/Main.jsx
@@ -4,10 +4,10 @@ const Main = () => {
     return (
         <div>
             <div className="flex justify-end bg-gradient-to-br from-green-400 to-violet-400 via-blue-400  p-9 rounded-b-2xl shadow-2xl">
-              <nav className="flex space-x-[100px] mt-5 justify-center font-light text-xl ">
-                 <Link to="/home" className="">Home</Link>
-                 <Link to="/aboutUs">About</Link>
-                 <Link to="/service">Service</Link>
+              <nav className="flex space-x-[100px] mt-5 justify-center font-light text-xl text-white">
+                 <Link to="/home" className="hover:underline transition duration-200 hover:scale-110">Home</Link>
+                 <Link to="/aboutUs" className="hover:underline transition duration-200 hover:scale-110">About</Link>
+                 <Link to="/service" className="hover:underline transition duration-200 hover:scale-110">Service</Link>
               </nav>
             </div>
         </div>
