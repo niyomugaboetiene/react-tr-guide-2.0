590d6850761df2fbb3916692c1dfeda4111c7b5f
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-21 08:20:07 +0200

    react: learning about routing

--- SWD5ROUTE/src/routing/project/Main.jsx
+++ SWD5ROUTE/src/routing/project/Main.jsx
@@ -1,1 +1,18 @@
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Service from "./Service";
+
+const Main = () => {
+    return (
+        <div>
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/service">Service</Link>
+        </nav>
+        </div>
+
+
+    )
+}
