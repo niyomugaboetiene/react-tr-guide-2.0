fa5bc3b88309bc500f818db210858c17eb336fdb
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-21 08:17:25 +0200

    react: learning about routing

---
+++ SWD5ROUTE/src/routing/project/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+        <p>This is About page which contain our team information</p>
+    </div>
+  )
+}
+
+export default About
