85d7d134675734f677b93c31bdc21cb042067c29
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-21 11:18:33 +0200

    react: learning about navigation

--- SWD5ROUTE/src/routing/project/Home.jsx
+++ SWD5ROUTE/src/routing/project/Home.jsx
@@ -3,9 +3,12 @@ import React from "react";
 const HomePage = () => {
   return (
     <div className="mt-10 ms-3">
-        <p className="font-light text-2xl ">This is Home page which contain greeting</p>
+        <p className="font-light text-2xl bg-gradient-to-tl from-purple-300 to-yellow-300 via-violet-400 bg-clip-text text-transparent">
+            This is Home page which contain greeting
+        </p>
     </div>
   )
 }
 
 export default HomePage
+// * adding gradient text "bg-gradient-to-tl from-purple-300 to-yellow-300 via-violet-400 bg-clip-text text-transparent"
