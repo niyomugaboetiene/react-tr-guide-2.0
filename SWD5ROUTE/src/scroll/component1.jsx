37df61fa3dd9d888c171f7730cba04af66141d27
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-24 14:48:02 +0200

    learning about scroll view

---
+++ SWD5ROUTE/src/scroll/component1.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Component1 = () => {
+  return (
+    <div className="h-screen bg-red-300 flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Component 1</h1>
+    </div>
+  );
+};
+
+export default Component1;
