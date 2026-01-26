d5408b01014961082aca2bb62f043eb649adebd9
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-01-26 11:58:59 +0200

    commiting reamining commits

--- SWD5ROUTE/src/main.jsx
+++ SWD5ROUTE/src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// import App from './App.jsx'
-import AppTest from './AppTest'
+import App from './App.jsx'
+// import AppTest from './AppTest'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <AppTest />
+    <App />
+    {/* <AppTest /> */}
   </StrictMode>,
 )
