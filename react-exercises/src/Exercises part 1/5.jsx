05b270a5051d012c6ce1f8976f5cd0dbd23edcaf
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-01-29 19:26:14 +0200

    performing exercises in react js

--- react-exercises/src/Exercises part 1/5.jsx
+++ react-exercises/src/Exercises part 1/5.jsx
@@ -4,12 +4,18 @@ const ChangeColor = () => {
     const [color, setColor] = useState("bg-blue-400");
 
     function change() {
-        setColor('bg-green-500')
+        setColor(prevColor =>
+            prevColor === 'bg-blue-400' ? 'bg-green-500' : 'bg-blue-400'
+        )
     }
 
     return (
-        <div className={`${color} min-h-screen`}>
-            <button onClick={ChangeColor}>Change color</button>
+        <div className={`${color} min-h-screen flex items-center justify-center`}>
+                 <button onClick={change} 
+                    className="bg-gray-500 text-white py-3 px-6 rounded-lg hover:bg-gray-600 transition-colors"
+                 >
+                    Change color
+                </button>
         </div>
     )
 }
