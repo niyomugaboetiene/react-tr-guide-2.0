34e96ad4b039b6df4db4f8222810b70feb128dcf
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-01-29 19:47:46 +0200

    performing exercises in react js

--- react-exercises/src/Exercises part 1/8.jsx
+++ react-exercises/src/Exercises part 1/8.jsx
@@ -5,7 +5,36 @@ function LoginFOrm () {
     const [password, setPassword] = useState("")
     const [message, setMessage] = useState("");
 
-    if (!username) {
-        setMessage("Username is required")
+    function handleSubmit() {
+       if (!username) {
+          setMessage("Username is required")
+       }
+    
+        if (!password) {
+            setMessage("Password is required")
+        }  
+    
+        setMessage("Login successfully");
+
     }
+
+    return (
+        <div>
+            <form  onSubmit={handleSubmit}>
+                <label htmlFor="">Usename</label> <br />
+                <input type="text"  onClick={(e) => setUsername(e.target.values)} />
+
+                <label htmlFor="">Password</label> <br />
+                <input type="text"  onClick={(e) => setPassword(e.target.values)} />
+
+                <button>Submit</button>
+
+                {message && (
+                    <p>{message}</p>
+                )}
+            </form>
+        </div>
+    )
 }
+
+export default LoginFOrm
