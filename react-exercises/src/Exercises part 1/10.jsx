70adaaff711fcb8e3227d627e6198e93398d37c5
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-02-02 19:41:37 +0200

    learning K-means clustering

--- react-exercises/src/Exercises part 1/10.jsx
+++ react-exercises/src/Exercises part 1/10.jsx
@@ -4,16 +4,16 @@ function KeyPressed() {
     const [lastKey, setLastKey] = useState("");
 
     return (
-        <div className="bg-green-500 min-h-screen">
-            <div>
-              <h1 className="text-center text-2xl">Write any key</h1>
+        <div className="bg-gray-500 min-h-screen flex justify-center items-center">
+            <div className="">
+              <h1 className="text-center text-2xl text-white font-bold">Write any key</h1>
                <input type="text" 
-                   className="border-2 "
+                   className="border w-full rounded px-6 py-1 mb-2 border-white text-white"
                    onKeyDown={(e) => setLastKey(e.key)}
                />
 
                {lastKey && (
-                   <p>You pressed: {lastKey}</p>
+                   <p className="text-white text-xl">You pressed: <strong>{lastKey}</strong></p>
                )}
             </div>
 
